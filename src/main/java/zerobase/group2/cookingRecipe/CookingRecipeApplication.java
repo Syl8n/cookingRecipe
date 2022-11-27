7f53864f6a58b08f78eba04108a9543c46926096
Syl8n <magi8520@gmail.com>
2022-11-27 22:42:03 +0900

    Initial commit

---
+++ src/main/java/zerobase/group2/cookingRecipe/CookingRecipeApplication.java
@@ -0,0 +1,13 @@
+package zerobase.group2.cookingRecipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookingRecipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookingRecipeApplication.class, args);
+	}
+
+}
